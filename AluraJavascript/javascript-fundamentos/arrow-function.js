4f1e63227b9c61541b485b5e37569b77e292e48c
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-06 09:38:14 -0300

    arrow func

--- AluraJavascript/javascript-fundamentos/arrow-function.js
+++ AluraJavascript/javascript-fundamentos/arrow-function.js
@@ -2,4 +2,11 @@ function apresentar(nome) {
   return `Me nome é ${nome}`;
 }
 
+//Arrow function (anônima)
+const apresentarArrowFunc = (nome) => `Meu nome é ${nome}`;
+
+console.log(apresentarArrowFunc(nome));
+
+const soma = (num1, num2) => num1 + num2;
+
 
