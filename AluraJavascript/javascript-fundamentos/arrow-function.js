8bbb501890b5751e262b7cdf082e58bdfd23fd86
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-06 09:39:12 -0300

    Arrow function com + de 1 linha de instrução

--- AluraJavascript/javascript-fundamentos/arrow-function.js
+++ AluraJavascript/javascript-fundamentos/arrow-function.js
@@ -9,4 +9,11 @@ console.log(apresentarArrowFunc(nome));
 
 const soma = (num1, num2) => num1 + num2;
 
-
+// Arrow function com + de 1 linha de instrução
+const somaNumerosMenores = (num1, num2) => {
+  if (num1 > 10 || num2 > 10) {
+    return "Somente números menores que 10.";
+  } else {
+    return num1 + num2;
+  }
+};
