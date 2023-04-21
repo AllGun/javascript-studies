363eccd6feef22715b68800d0681cd8fbcc4f0a2
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-21 08:18:00 -0300

    cap 2 exercicio 2a done

--- livro-js/cap-02/js/js_exercicios_cap_2/exercicio_cap_2a.js
+++ livro-js/cap-02/js/js_exercicios_cap_2/exercicio_cap_2a.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+const inputForm = document.querySelector("form");
+const responsePreco = document.querySelector("#outPreco");
+const responseDesconto = document.querySelector("#outDesconto");
+
+inputForm.addEventListener("submit", (e) => {
+  const medicamento = inputForm.inMedicamento.value;
+  const preco = Number(inputForm.inPreco.value);
+
+  console.log(medicamento);
+  console.log(preco);
+
+  const desconto = preco * 2 * 0.9;
+
+  console.log(desconto);
+
+  responsePreco.innerText = `R$${preco}`;
+  responseDesconto.innerText = `R$${desconto}`;
+
+  e.preventDefault();
+});
