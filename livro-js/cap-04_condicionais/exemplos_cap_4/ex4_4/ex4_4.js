0a4bedb95fea410028c46ee9df61f60ee3824cab
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-23 08:41:21 -0300

    ex4_4 done

--- livro-js/cap-04_condicionais/exemplos_cap_4/ex4_4/ex4_4.js
+++ livro-js/cap-04_condicionais/exemplos_cap_4/ex4_4/ex4_4.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+// cria referência ao form e elemento onde será exibida a resposta
+const outForm = document.querySelector("form");
+const response = document.querySelector("#outHoraFranca");
+
+// "ouvinte" de evento, acionado quando enviar o form
+outForm.addEventListener("submit", (e) => {
+  // evita o envio do form
+  e.preventDefault();
+
+  // pega e converte o conteúdo do campo inHoraBrasil
+  const horaBrasil = Number(outForm.inHoraBrasil.value);
+
+  // calc o horário na França
+  let horaFranca = horaBrasil + 5;
+
+  // se passar das 24h na França
+  if (horaFranca > 24) {
+    horaFranca = horaFranca - 24;
+  }
+
+  // exibe a resposta (altera o conteúdo do elemento h3)
+  response.innerText = `${horaFranca.toFixed(2)}`;
+});
