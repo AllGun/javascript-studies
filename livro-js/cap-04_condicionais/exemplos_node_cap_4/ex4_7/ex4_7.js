051913800b80c3e3ee45bb0cedba3a45142d3539
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-23 10:53:15 -0300

    node ex4_7

---
+++ livro-js/cap-04_condicionais/exemplos_node_cap_4/ex4_7/ex4_7.js
@@ -0,0 +1,16 @@
+// add pacote prompt-sync
+const prompt = require("prompt-sync")();
+
+// entrada de dados
+const pessoas = Number(prompt("Número de pessoas: "));
+const peixes = Number(prompt("Número de peixes: "));
+
+let pagar;
+
+if (peixes <= pessoas) {
+    pagar = pessoas * 20;
+} else {
+    pagar = (pessoas * 20) + ((peixes - pessoas) * 12)
+}
+
+console.log(`Pagar R$${pagar.toFixed(2)}`);
