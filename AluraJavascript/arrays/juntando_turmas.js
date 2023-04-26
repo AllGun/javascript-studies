4340c644429ce9c01cc64e89dbb1fd2a20bf6b45
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-25 23:55:49 -0300

    array salaUnificada

--- AluraJavascript/arrays/juntando_turmas.js
+++ AluraJavascript/arrays/juntando_turmas.js
@@ -2,3 +2,5 @@ const pythonClass = ["Alan","Bruna", "Camila", "Daiane"];
 const javaScriptClass = ["Marcos", "Tiago", "Leidiane", "Zoraia"];
 
 const salaUnificada = pythonClass.contat(javaScriptClass);
+
+console.log(salaUnificada);
