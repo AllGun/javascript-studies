3db577781adc34bf23689e594947df6a855853ca
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-25 23:59:23 -0300

    concat()

--- AluraJavascript/arrays/juntando_turmas.js
+++ AluraJavascript/arrays/juntando_turmas.js
@@ -1,5 +1,5 @@
 //Unindo 2 turmas de um curso em uma unica turma
-// usando o método concat()
+// usando o método concat() para juntar as turmas
 
 const pythonClass = ["Alan","Bruna", "Camila", "Daiane"];
 const javaScriptClass = ["Marcos", "Tiago", "Leidiane", "Zoraia"];
