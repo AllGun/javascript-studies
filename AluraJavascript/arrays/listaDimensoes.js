836121b41d88ec1624efbb7b57904195665a150f
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-26 00:07:05 -0300

    Lista de 2 dimessoes

---
+++ AluraJavascript/arrays/listaDimensoes.js
@@ -0,0 +1,18 @@
+/*
+Desafio - Lista com 2 dimensões
+
+    Crie uma lista com os seguintes alunos:
+    - João, Juliana, Caio e Ana
+
+    Crie uma lista com as seguintes médias
+    - 10, 8, 7.5 e 9
+
+    Crie uma lista com 2 dimensões com os nomes e as médias
+*/
+
+const alunos = ["Jõa", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+
+let listaAlunoseMedias = [alunos, medias];
+
+console.log(listaAlunoseMedias);
