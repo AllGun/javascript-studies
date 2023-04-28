c105b91a2f019f3e0d8119c05fae6f6cb7008af4
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-27 23:22:09 -0300

    calc media done

--- AluraJavascript/arrays/mediaFor.js
+++ AluraJavascript/arrays/mediaFor.js
@@ -1,7 +1,13 @@
-const notas = [10, 6.7, 8, 9]
+const notas = [10, 6.7, 8, 9];
 
-let somaDasNotas = null;
+let somaDasNotas = 0;
 
-for(let i; i <= notas.length; i++){
-
+for (let i = 0; i < notas.length; i++) {
+  somaDasNotas += notas[i];
 }
+
+let media = somaDasNotas / notas.length;
+
+console.log(media);
+console.log(somaDasNotas);
+console.log(notas);
