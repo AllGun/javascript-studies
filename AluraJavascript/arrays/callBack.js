f9a615bbea55c955aa4207a69f8915f809c7b937
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-28 05:28:39 -0300

    call back exemplo

---
+++ AluraJavascript/arrays/callBack.js
@@ -0,0 +1,28 @@
+const alunos = [
+    "Joã",
+    "Juliana",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Aline",
+    "Fabiana",
+    "André",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isabela",
+    "Vinny",
+    "Renan",
+    "Renata",
+    "Daisy",
+    "Camilo",
+  ];
+
+  alunos.forEach(printNames);
+
+  function printNames(nome){
+    console.log(nome);
+  }
