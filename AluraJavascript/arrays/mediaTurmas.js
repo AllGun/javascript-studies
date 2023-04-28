5bdafa696c6021340d8b46c30cfc7cba0b40a372
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-28 07:26:09 -0300

    media das salas

---
+++ AluraJavascript/arrays/mediaTurmas.js
@@ -0,0 +1,8 @@
+/* Desafio - medias das turmar
+    Com a média de todos os alunos de 3 salas,
+    calcule a média geral de cada sala
+    */
+
+const salaJavaScript = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava = [6, 5, 8, 9, 5, 6]
+const salaPython = [7, 3.5, 8, 9.5];
