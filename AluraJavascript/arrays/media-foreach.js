6fc27caf339d70a883894c2b76341c3784b7e44e
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-28 05:21:58 -0300

    percorrendo array com forEach

--- AluraJavascript/arrays/media-foreach.js
+++ AluraJavascript/arrays/media-foreach.js
@@ -4,8 +4,18 @@ let somaNotas = 0;
 
 notas.forEach( nota => {
     somaNotas += nota;
+    console.log(nota)
 })
 
 let media = somaNotas / notas.length;
 
 console.log(media);
+
+
+//Percorrendo o array com forEach
+
+const alunos = ["Camila", "Bia", "Juan", "Bruno", "Guilherme", "Stéfanii"];
+
+alunos.forEach( aluno => {
+    console.log(aluno);
+})
