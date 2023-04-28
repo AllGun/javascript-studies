27373becab2e171ea085b89e05013b304080895e
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2023-04-28 05:40:17 -0300

    pradronizando nomes com .map

---
+++ AluraJavascript/arrays/pradronizandoNomes.js
@@ -0,0 +1,10 @@
+let nomes = ["ana juLIa", "CaiO vinicios", "BIA silva"];
+
+const nomesAtualizados = nomes.map( nome => nome.toUpperCase());
+
+console.log(nomesAtualizados);
+
+const nomesMinusculo = nomes.map(nome => nome.toLocaleLowerCase());
+
+console.log(nomesMinusculo);
+
