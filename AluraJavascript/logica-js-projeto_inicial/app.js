da873935e8481946889e001220266717fb2b0da8
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-01-25 07:50:51 -0300

    Commits - Dia 25

--- AluraJavascript/logica-js-projeto_inicial/app.js
+++ AluraJavascript/logica-js-projeto_inicial/app.js
@@ -1,9 +1,28 @@
 alert("Boas vindas ao jogo do número secredo");
 let numeroSecreto = 29;
-let numeroUsuario = prompt("Escolha um número entre 1 e 100:");
+let numeroUsuario,
+  tentativas = 1;
+console.log(numeroSecreto == numeroUsuario);
+console.log(typeof numeroUsuario);
+console.log(numeroSecreto);
 
-if (numeroUsuario == numeroSecreto) {
-  alert("Parabéns!");
-} else {
-  alert("Você errou o número! Tente outra vez!");
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Escolha um número entre 1 e 100:");
+  if (numeroUsuario == numeroSecreto) {
+    alert(
+      `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`
+    );
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert(`O número secreto é menor que ${numeroUsuario}.`);
+    } else {
+      alert(`O número secreto é maior que ${numeroUsuario}.`);
+    }
+    tentativas++;
+  }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tantativa";
+
+console.log(numeroSecreto == numeroUsuario);
+console.log(tentativas);
